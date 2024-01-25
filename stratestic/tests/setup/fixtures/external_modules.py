1a99be72409d8c7ad742c32da23c640b77e3c14e
Diogo Matos Chaves <diogo_chaves@hotmail.com>
2024-01-25 15:53:51 -0300

    Add tests for Trader class

--- stratestic/tests/setup/fixtures/external_modules.py
+++ stratestic/tests/setup/fixtures/external_modules.py
@@ -1,3 +1,5 @@
+import logging
+
 import matplotlib
 import plotly
 import pytest
@@ -6,3 +8,9 @@ import pytest
 @pytest.fixture
 def mocked_plotly_figure_show(mocker):
     mocker.patch.object(plotly.graph_objs.Figure, "show", lambda self: None)
+
+
+@pytest.fixture
+def spy_logging_info(mocker):
+    return mocker.spy(logging, "info")
+
---
+++ stratestic/tests/utils/test_trading.py
@@ -0,0 +1,65 @@
+import pytest
+
+from stratestic.trading import Trader
+from stratestic.tests.setup.fixtures.external_modules import spy_logging_info
+
+
+class TestTrader:
+
+    @pytest.mark.parametrize(
+        "method,extra_input",
+        [
+            pytest.param(
+                '_set_position',
+                ['BTCUSDT', 1],
+                id="_set_position",
+            ),
+            pytest.param(
+                '_get_position',
+                ['BTCUSDT'],
+                id="_get_position",
+            ),
+            pytest.param(
+                'buy_instrument',
+                ['BTCUSDT'],
+                id="buy_instrument",
+            ),
+            pytest.param(
+                'sell_instrument',
+                ['BTCUSDT'],
+                id="sell_instrument",
+            ),
+            pytest.param(
+                'close_pos',
+                ['BTCUSDT'],
+                id="close_pos",
+            ),
+        ],
+    )
+    def test_exceptions_without_args(self, method, extra_input):
+
+        trader = Trader(1000)
+
+        with pytest.raises(NotImplementedError):
+            getattr(trader, method)(*extra_input)
+
+    @pytest.mark.parametrize(
+        "method",
+        [
+            pytest.param(
+                'print_current_position_value',
+                id="_set_position",
+            ),
+            pytest.param(
+                'print_current_nav',
+                id="_get_position",
+            ),
+        ],
+    )
+    def test_print_functions(self, method, spy_logging_info):
+
+        trader = Trader(1000)
+
+        getattr(trader, method)('', 10000)
+
+        assert spy_logging_info.call_count == 1
